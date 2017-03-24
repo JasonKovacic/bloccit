b1394f5386f84ecfc65d9a0d4e24b512c9b8e749
Jason <kovacic.jason@gmail.com>
2017-03-23 17:41:12 -0700

    Checkpoint 14 Completed

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -8,14 +8,14 @@ RSpec.describe WelcomeController, type: :controller do
   # #3
         expect(response).to render_template("index")
       end
-  end
+    end
 
 
    describe "GET about" do
      it "renders the about template" do
        get :about
        expect(response).to render_template("about")
-     end
+    end
    end
 
    describe "GET faq" do
