3b0cfe2a45737cfc71aa9e6ec3fa2d626a630095
Jason <kovacic.jason@gmail.com>
2017-04-06 12:01:01 -0700

    Assignment 19 Completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328183202) do
+ActiveRecord::Schema.define(version: 20170405001920) do
+
+  create_table "advertisements", force: :cascade do |t|
+    t.string   "title"
+    t.text     "copy"
+    t.integer  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
