bf2ef97ca46377c975460d0e59fc68d095b517ac
Jason <kovacic.jason@gmail.com>
2017-06-02 02:18:45 -0700

    Checkpoint 25 Authentication Completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
     resources :topics do
     resources :posts, except: [:index]
 end
-
+    resources :users
     get 'about' => 'welcome#about'
     root 'welcome#index'
 
