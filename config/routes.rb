65745b8e0d95a8fb76dbb3d999bce161ead9cd83
Jason <kovacic.jason@gmail.com>
2017-07-27 16:07:11 -0400

    Checkpoint 26 Completed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
     resources :topics do
     resources :posts, except: [:index]
 end
-    resources :users
+    resources :users, only: [:new, :create]
+
+    resources :sessions, only: [:new, :create, :destroy]
+
     get 'about' => 'welcome#about'
     root 'welcome#index'
 
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -11,71 +11,76 @@ RSpec.describe UsersController, type: :controller do
        end
 
 
-describe "GET new" do
-   it "returns http success" do
-     get :new
-     expect(response).to have_http_status(:success)
-   end
+    describe "GET new" do
+       it "returns http success" do
+         get :new
+         expect(response).to have_http_status(:success)
+       end
 
-   it "instantiates a new user" do
-     get :new
-     expect(assigns(:user)).to_not be_nil
-   end
- end
+       it "instantiates a new user" do
+         get :new
+         expect(assigns(:user)).to_not be_nil
+       end
+     end
 
- describe "POST create" do
-    it "returns an http redirect" do
-      post :create, user: new_user_attributes
-      expect(response).to have_http_status(:redirect)
-    end
+     describe "POST create" do
+        it "returns an http redirect" do
+          post :create, user: new_user_attributes
+          expect(response).to have_http_status(:redirect)
+        end
 
-# #4
-    it "creates a new user" do
-      expect{
-        post :create, user: new_user_attributes
-      }.to change(User, :count).by(1)
-    end
+    # #4
+        it "creates a new user" do
+          expect{
+            post :create, user: new_user_attributes
+          }.to change(User, :count).by(1)
+        end
 
-# #5
-    it "sets user name properly" do
-      post :create, user: new_user_attributes
-      expect(assigns(:user).name).to eq new_user_attributes[:name]
-    end
+    # #5
+        it "sets user name properly" do
+          post :create, user: new_user_attributes
+          expect(assigns(:user).name).to eq new_user_attributes[:name]
+        end
 
-# #6
-    it "sets user email properly" do
-      post :create, user: new_user_attributes
-      expect(assigns(:user).email).to eq new_user_attributes[:email]
-    end
+    # #6
+        it "sets user email properly" do
+          post :create, user: new_user_attributes
+          expect(assigns(:user).email).to eq new_user_attributes[:email]
+        end
 
-# #7
-    it "sets user password properly" do
-      post :create, user: new_user_attributes
-      expect(assigns(:user).password).to eq new_user_attributes[:password]
-    end
+    # #7
+        it "sets user password properly" do
+          post :create, user: new_user_attributes
+          expect(assigns(:user).password).to eq new_user_attributes[:password]
+        end
 
-# #8
-    it "sets user password_confirmation properly" do
-      post :create, user: new_user_attributes
-      expect(assigns(:user).password_confirmation).to eq new_user_attributes[:password_confirmation]
-    end
-  end
+    # #8
+        it "sets user password_confirmation properly" do
+          post :create, user: new_user_attributes
+          expect(assigns(:user).password_confirmation).to eq new_user_attributes[:password_confirmation]
+        end
 
-  def create
-# #9
-    @user = User.new
-    @user.name = params[:user][:name]
-    @user.email = params[:user][:email]
-    @user.password = params[:user][:password]
-    @user.password_confirmation = params[:user][:password_confirmation]
+        it "logs the user in after sign up" do
+           post :create, user: new_user_attributes
+           expect(session[:user_id]).to eq assigns(:user).id
+         end
+      end
 
-# #10
-    if @user.save
-      flash[:notice] = "Welcome to Bloccit #{@user.name}!"
-      redirect_to root_path
-    else
-      flash.now[:alert] = "There was an error creating your account. Please try again."
-      render :new
-    end
-  end
+      def create
+    # #9
+        @user = User.new
+        @user.name = params[:user][:name]
+        @user.email = params[:user][:email]
+        @user.password = params[:user][:password]
+        @user.password_confirmation = params[:user][:password_confirmation]
+
+    # #10
+        if @user.save
+          flash[:notice] = "Welcome to Bloccit #{@user.name}!"
+          redirect_to root_path
+        else
+          flash.now[:alert] = "There was an error creating your account. Please try again."
+          render :new
+        end
+      end
 end
